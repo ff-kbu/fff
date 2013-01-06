66cbc8fb2ea4a9fc3d318ca85e3a1d58ab55e0c1
Jan Luehr <jluehr@gmx.net>
2013-01-06 17:16:21 +0100

    Push selected tag only

--- create_tags.sh
+++ create_tags.sh
@@ -1,10 +1,10 @@
 tagname=$1
 git tag -a $1 -m "Created by Jenkins build for $1"
-git push $1
+git push origin $1
 for  feed in packages luci fastd config_mode
 do
 	cd feeds/$feed 
 	git tag -a $1 -m "Created by Jenkins build for $1" 
-	git push $1
+	git push origin $1
 	cd ../..
 done
