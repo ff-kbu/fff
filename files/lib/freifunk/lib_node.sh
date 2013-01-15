4293d28b165795cb81c0784215e2b094d643730a
Jan Luehr <jluehr@gmx.net>
2013-01-15 11:38:09 +0100

    Refacotring: extract get_main_address, merge with lib_node.sh

--- files/lib/freifunk/lib_node.sh
+++ files/lib/freifunk/lib_node.sh
@@ -1,21 +1,16 @@
-node_mac(){
-        local board="$(ar71xx_board_name)"
-        local macaddr=""
-        case "$board" in
-        tl-wdr3600|\
-        tl-wdr4300)
-                macaddr="$(uci get wireless.radio1.macaddr)"
-                ;;
+get_main_address() {
+    case "$board" in
+        tl-wdr3600|tl-wdr4300)
+            uci get wireless.radio1.macaddr
+            ;;
         *)
-                macaddr="$(uci get wireless.radio0.macaddr)"
-                ;;
-        esac
-        echo $macaddr
+            uci get wireless.radio0.macaddr
+            ;;
+    esac
 }
 
-# Convert mac-address to node_id
-node_id(){
-        local macaddr=$(node_mac)
+get_node_id(){
+	macaddr=$(get_main_address)
         echo ${macaddr//:/}
 }
 
