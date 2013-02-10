ab4e42b7cb8c2156a9487d90cc9fa7480d5f099e
yanosz <yanosz@gmx.net>
2013-02-10 12:18:17 +0100

    Tag watchdog, too.

--- create_tags.sh
+++ create_tags.sh
@@ -1,7 +1,7 @@
 tagname=$1
 git tag -a $1 -m "Created by Jenkins build for $1"
 git push origin $1
-for  feed in packages luci fastd config_mode
+for  feed in packages luci fastd config_mode watchdog
 do
 	cd feeds/$feed 
 	git tag -a $1 -m "Created by Jenkins build for $1" 
