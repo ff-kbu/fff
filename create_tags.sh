0ca831182b525114c562a297b9bbf3ea439a13ce
kaleng <kaleng@gokk.de>
2014-04-02 18:23:58 +0200

    add create_tags script

---
+++ create_tags.sh
@@ -0,0 +1,10 @@
+tagname=$1
+git tag -a $1 -m "Created by Jenkins build for $1"
+git push origin $1
+for  feed in packages luci fastd config_mode ath9k_watchdog routing
+do
+	cd feeds/$feed 
+	git tag -a $1 -m "Created by Jenkins build for $1" 
+	git push origin $1
+	cd ../..
+done
