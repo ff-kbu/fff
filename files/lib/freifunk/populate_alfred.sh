ba2fb0abf6f995876361525e6d7cc6488a9df8ec
yanosz <yanosz@gmx.net>
2014-08-04 01:28:48 +0200

    Restart alfred / It's lib in Barrier Breaker

--- files/lib/freifunk/populate_alfred.sh
+++ files/lib/freifunk/populate_alfred.sh
@@ -3,6 +3,8 @@
 # Data will vanish after 10min by design of alfred
 # Thus this script is be called every 5 minutes
 
+/etc/init.d/alfred restart
+
 version=$(cat /etc/freifunk_version)
 mac=$(uci get freifunk.@node[0].nodeid)
 machine=$(cat /tmp/sysinfo/model)
@@ -11,7 +13,7 @@ echo $version | /usr/sbin/alfred -s 94
 echo $mac |  /usr/sbin/alfred -s 192
 echo $machine | /usr/sbin/alfred -s 110
 
-. /etc/functions.sh
+. /lib/functions.sh
 . /lib/ar71xx.sh
 tp_model=$(tplink_get_hwid)
 echo $tp_model | /usr/sbin/alfred -s 158
