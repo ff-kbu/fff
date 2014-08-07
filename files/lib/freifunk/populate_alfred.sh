ecdf6cef9e3482767d68f76611c73dce1c5231f0
yanosz <yanosz@gmx.net>
2014-08-07 13:09:23 +0200

    Restart unresponsive alfred-daemon, if needed

--- files/lib/freifunk/populate_alfred.sh
+++ files/lib/freifunk/populate_alfred.sh
@@ -3,6 +3,16 @@
 # Data will vanish after 10min by design of alfred
 # Thus this script is be called every 5 minutes
 
+# Test - if alfred is crashed / frozen, restart
+# No data -> return code 0, too
+/usr/sbin/alfred -r 94
+
+#Check for non-zero return code
+if [ $? -ne 0 ]; then
+    /etc/init.d/alfred restart 
+fi
+
+
 version=$(cat /etc/freifunk_version)
 mac=$(uci get freifunk.@node[0].nodeid)
 machine=$(cat /tmp/sysinfo/model)
