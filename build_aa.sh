81cf3cf3886f2cfadff222df344159712a26b4f0
yanosz <yanosz@gmx.net>
2014-12-10 00:21:36 +0100

    No bashism in AA

--- build_aa.sh
+++ build_aa.sh
@@ -13,7 +13,7 @@ cp ../configs/config-aa .config
 # Version-Strings codieren
 #make CONFIG_VERSION_NUMBER="${BUILD_ID} (ff-kbu-master-aa-continuous)" -j16
 #make clean
-if [[ -z "$VERSION" ]]; then
+if [ -z "$VERSION" ]; then
 	VERSION="${BUILD_ID}"
 fi
 echo "${VERSION}-legacy" > files/etc/freifunk_version        
